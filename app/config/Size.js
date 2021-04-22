a68574c13ba5018595a41a8ca7a3cf356d6784b9
Alexander Bell <contact@alexanderbell.info>
2021-04-21 23:16:47 -0700

    Create Size.js

---
+++ app/config/Size.js
@@ -0,0 +1,29 @@
+/**
+ * Copyright (c) Lineage, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow strict-local
+ * @format
+ */
+
+'use strict';
+
+import { Dimensions } from 'react-native';
+
+const { width, height } = Dimensions.get('window');
+
+export default {
+  s1:  2,
+  s2:  3,
+  s3:  5,
+  s4:  8,
+  s5: 13,
+  s6: 21,
+  s7: 34,
+  s8: 55,
+
+  width,
+  height,
+}
