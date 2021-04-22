aa693b5756d0e17704199f4e3385283f6078c80e
Alexander Bell <contact@alexanderbell.info>
2021-04-21 23:16:57 -0700

    Create Font.js

---
+++ app/config/Font.js
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) Lineage, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow strict-local
+ * @format
+ */
+
+'use strict';
+
+import Size from './Size';
+
+export default {
+  body: {
+    fontFamily: "-apple-system, BlinkMacSystemFont",
+    fontSize: Size.s5,
+    lineHeight: Size.s6,
+  },
+  title: {
+    fontFamily: Platform.OS === 'macos' ? "Baskerville" : "Century Schoolbook",
+    fontSize: Size.s7,
+    lineHeight: Size.s8,
+  },
+  heading: {
+    fontFamily: Platform.OS === 'macos' ? "Baskerville" : "Century Schoolbook",
+    fontSize: Size.s6,
+    lineHeight: Size.s7,
+  }
+}
