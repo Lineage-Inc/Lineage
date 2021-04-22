e406811cf0e8e78b761968ea9a55c849fcd3d178
Alexander Bell <contact@alexanderbell.info>
2021-04-21 23:16:42 -0700

    Create Colors.js

---
+++ app/config/Colors.js
@@ -0,0 +1,20 @@
+/**
+ * Copyright (c) Lineage, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow strict-local
+ * @format
+ */
+
+'use strict';
+
+export default {
+  primary: '#009444',
+  white: '#FFF',
+  lighter: '#F3F3F3',
+  light: '#DAE1E7',
+  dark: '#444',
+  black: '#000',
+};
