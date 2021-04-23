b077c430f0f4df9eb4e9f0dfd265bb0953a44c63
Alexander Bell <contact@alexanderbell.info>
2021-04-22 22:14:20 -0700

    Update button styles config

--- app/components/Button/styles.js
+++ app/components/Button/styles.js
@@ -1,17 +1,17 @@
 import { StyleSheet } from 'react-native';
-import Colors from '../../config/Colors';
+import { Theme } from 'Config';
 
 export default StyleSheet.create({
   button: {
     paddingVertical: 10,
     paddingHorizontal: 20,
-    backgroundColor: Colors.primary,
+    backgroundColor: Theme.colors.primary,
     margin: 5,
     borderRadius: 3,
     width: 150,
   },
   buttonText: {
-    color: Colors.black,
+    color: Theme.colors.white,
     fontSize: 16,
     fontWeight: '500',
     textAlign: 'center',
