e9ec3cb78d8de8251cecf784c8cdbbc40b7e69bb
Alexander Bell <contact@alexanderbell.info>
2022-10-15 00:27:24 -0700

    Link font assets

--- storybook/stories/index.js
+++ storybook/stories/index.js
@@ -1,6 +1,6 @@
 import './Button/Button.stories';
 import './Icon/Icon.stories';
 import './Welcome/Welcome.stories';
-import './Parameters/Parameters.stories';
 import './Typography/Heading.stories';
 import './Typography/Paragraph.stories';
+import '../../app/components/Nav/Nav.stories';
