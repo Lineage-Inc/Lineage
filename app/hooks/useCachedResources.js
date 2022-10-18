a37eb229b4b692f11b2feabe7ca56a54eacae954
Alexander Bell <contact@alexanderbell.info>
2022-10-17 23:54:08 -0700

    Update useCachedResources.js

--- app/hooks/useCachedResources.js
+++ app/hooks/useCachedResources.js
@@ -1,17 +1,31 @@
-async function loadResourcesAndDataAsync() {
-	try {
-		SplashScreen.preventAutoHideAsync();
+import * as React from 'react';
 
-		// Load fonts
-		await Font.loadAsync({
-			'Archivo-Italic': require('../assets/font/Archivo-Italic[wdth,wght].ttf'),
-			Archivo: require('../assets/font/Archivo[wdth,wght].ttf'),
-		});
-	} catch (e) {
-		// We might want to provide this error information to an error reporting service
-		console.warn(e);
-	} finally {
-		setLoadingComplete(true);
-		SplashScreen.hideAsync();
-	}
+export default function useCachedResources() {
+	const [isLoadingComplete, setLoadingComplete] = React.useState(false);
+
+	// Load any resources or data that we need prior to rendering the app
+	React.useEffect(() => {
+		async function loadResourcesAndDataAsync() {
+			try {
+				SplashScreen.preventAutoHideAsync();
+
+				// Load fonts
+				await Font.loadAsync({
+					'Archivo Semibold': require('../../assets/font/Archivo-Italic[wdth,wght].ttf'),
+					Archivo: require('../../assets/font/Archivo[wdth,wght].ttf'),
+				});
+			} catch (e) {
+				// We might want to provide this error information to an error reporting service
+				console.warn(e);
+			} finally {
+				console.log(Archivo);
+				setLoadingComplete(true);
+				SplashScreen.hideAsync();
+			}
+		}
+
+		loadResourcesAndDataAsync();
+	}, []);
+
+	return isLoadingComplete;
 }
